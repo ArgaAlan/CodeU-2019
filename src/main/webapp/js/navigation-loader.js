4e7df1338ea52c7fc28c9372ce23a7ad1e61e13d
Drew Bernard <drewbernard@utexas.edu>
2019-03-05 14:55:36 -0600

    Initial feed.html commit for Public Feed (#24)

--- src/main/webapp/js/navigation-loader.js
+++ src/main/webapp/js/navigation-loader.js
@@ -35,8 +35,14 @@ function addLoginOrLogoutLinkToNavigation() {
               '/user-page.html?user=' + loginStatus.username, 'Your Page')));
 
           navigationElement.appendChild(
+          	  createListItem(createLink('/feed.html', 'Public Feed')));
+
+          navigationElement.appendChild(
               createListItem(createLink('/logout', 'Logout')));
         } else {
+          navigationElement.appendChild(
+          	  createListItem(createLink('/feed.html', 'Public Feed')));
+
           navigationElement.appendChild(
               createListItem(createLink('/login', 'Login')));
         }
