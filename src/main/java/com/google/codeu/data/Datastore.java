3ef50446375b530e855a2abecb1600c8e0f9f9d5
Alan Gonzalez <30873892+ArgaAlan@users.noreply.github.com>
2019-03-11 12:01:47 -0600

    Average and biggest message added (#22)

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -1,21 +1,22 @@
 /*
  * Copyright 2019 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.google.codeu.data;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -24,9 +25,6 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.SortDirection;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 /** Provides access to the data stored in Datastore. */
 public class Datastore {
@@ -52,98 +50,95 @@ public class Datastore {
    * Gets messages posted by a specific user.
    *
    * @return a list of messages posted by the user, or empty list if user has never posted a
-   *     message. List is sorted by time descending.
-   *     Messages are public and will display on recipient's user page.
+   *         message. List is sorted by time descending. Messages are public and will display on
+   *         recipient's user page.
    */
+
   public List<Message> getMessages(String recipient) {
-    Query query =
-        new Query("Message")
-            .setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
-            .addSort("timestamp", SortDirection.DESCENDING);
+    Query query = new Query("Message")
+        .setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
+        .addSort("timestamp", SortDirection.DESCENDING);
+
     PreparedQuery results = datastore.prepare(query);
-  
-   return convertEntitiesToMessages(results);
+
+    return convertEntitiesToMessages(results);
   }
 
   /**
    * Gets messages posted by all users.
    *
    * @return a list of messages posted by all users, or empty list if no user has ever posted a
-   *     message. List is sorted by time descending.
+   *         message. List is sorted by time descending.
    */
-public List<Message> getAllMessages(){
+  public List<Message> getAllMessages() {
 
-  Query query = new Query("Message")
-    .addSort("timestamp", SortDirection.DESCENDING);
-  PreparedQuery results = datastore.prepare(query);
+    Query query = new Query("Message").addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
 
-  return convertEntitiesToMessages(results);
- }
+    return convertEntitiesToMessages(results);
+  }
 
   /**
    * Gets messages of all useres specified by s/useres/users/
    *
    * @return null, updates List<Message> messages
    */
-public List<Message> convertEntitiesToMessages(PreparedQuery results){
-  List<Message> messages = new ArrayList<>();
 
-  for (Entity entity : results.asIterable()) {
-    try {
-      String idString = entity.getKey().getName();
-      UUID id = UUID.fromString(idString);
-      String user = (String) entity.getProperty("user");
-      String text = (String) entity.getProperty("text");
-      long timestamp = (long) entity.getProperty("timestamp");
-      String recipient = (String) entity.getProperty("recipient");
-      Message message = new Message(id, user, text, timestamp, recipient);
-      messages.add(message);
-    } catch (Exception e) {
-      System.err.println("Error reading message.");
-      System.err.println(entity.toString());
-      e.printStackTrace();
+  public List<Message> convertEntitiesToMessages(PreparedQuery results) {
+    List<Message> messages = new ArrayList<>();
+
+    for (Entity entity : results.asIterable()) {
+      try {
+        String idString = entity.getKey().getName();
+        UUID id = UUID.fromString(idString);
+        String user = (String) entity.getProperty("user");
+        String text = (String) entity.getProperty("text");
+        long timestamp = (long) entity.getProperty("timestamp");
+        String recipient = (String) entity.getProperty("recipient");
+        Message message = new Message(id, user, text, timestamp, recipient);
+        messages.add(message);
+      } catch (Exception e) {
+        System.err.println("Error reading message.");
+        System.err.println(entity.toString());
+        e.printStackTrace();
+      }
     }
+    return messages;
+
   }
-  return messages;
-}
-  
+
   /** Returns the total number of messages for all users. */
-  public int getTotalMessageCount(){
+  public int getTotalMessageCount() {
     Query query = new Query("Message");
     PreparedQuery results = datastore.prepare(query);
     return results.countEntities(FetchOptions.Builder.withLimit(1000));
   }
 
   /**
-  * Stores the User in Datastore Entity
-  */
- public void storeUser(User user) {
-  Entity userEntity = new Entity("User", user.getEmail());
-  userEntity.setProperty("email", user.getEmail());
-  userEntity.setProperty("aboutMe", user.getAboutMe());
-  datastore.put(userEntity);
- }
-
- /**
-  * Returns the User of email address with aboutMe, or
-  * null if no matching User was found.
-  */
- public User getUser(String email) {
-  Query query = new Query("User") .setFilter(new Query.FilterPredicate(
-                                                        "email",
-                                                        FilterOperator.EQUAL,
-                                                        email
-                                                      )
-                                            );
-  PreparedQuery results = datastore.prepare(query);
-  Entity userEntity = results.asSingleEntity();
-  if (userEntity == null) {
-   return null;
+   * Stores the User in Datastore Entity
+   */
+  public void storeUser(User user) {
+    Entity userEntity = new Entity("User", user.getEmail());
+    userEntity.setProperty("email", user.getEmail());
+    userEntity.setProperty("aboutMe", user.getAboutMe());
+    datastore.put(userEntity);
   }
 
-  //Return aboutMe
-  String aboutMe = (String) userEntity.getProperty("aboutMe");
-  User user = new User((String)userEntity.getProperty("email"), aboutMe);
-  return user;
- }
+  /**
+   * Returns the User of email address with aboutMe, or null if no matching User was found.
+   */
+  public User getUser(String email) {
+    Query query = new Query("User")
+        .setFilter(new Query.FilterPredicate("email", FilterOperator.EQUAL, email));
+    PreparedQuery results = datastore.prepare(query);
+    Entity userEntity = results.asSingleEntity();
+    if (userEntity == null) {
+      return null;
+    }
+
+    // Return aboutMe
+    String aboutMe = (String) userEntity.getProperty("aboutMe");
+    User user = new User((String) userEntity.getProperty("email"), aboutMe);
+    return user;
+  }
 }
--- src/main/java/com/google/codeu/servlets/StatsPageServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -1,12 +1,12 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
-
+import java.util.HashMap;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.google.codeu.data.Datastore;
 import com.google.gson.JsonObject;
 
@@ -14,7 +14,7 @@ import com.google.gson.JsonObject;
  * Handles fetching site statistics.
  */
 @WebServlet("/stats")
-public class StatsPageServlet extends HttpServlet{
+public class StatsPageServlet extends HttpServlet {
 
   private Datastore datastore;
 
@@ -27,15 +27,74 @@ public class StatsPageServlet extends HttpServlet{
    * Responds with site statistics in JSON.
    */
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     response.setContentType("application/json");
 
+    // List of all messages
+    List<com.google.codeu.data.Message> allmessages = datastore.getAllMessages();
+
+    // Json for the JS
+    JsonObject jsonObject = new JsonObject();
+
+    // Number of messages
     int messageCount = datastore.getTotalMessageCount();
 
-    JsonObject jsonObject = new JsonObject();
+    // Number of messages property added to the Json
     jsonObject.addProperty("messageCount", messageCount);
+
+    // Store of the biggest message
+    com.google.codeu.data.Message biggest = ((allmessages.size() == 0) ? null : allmessages.get(0));
+
+    /*
+     * countM, total number of messages users, register of all users
+     */
+    int countM = 0;
+    HashMap<String, Integer> usersAndNumOfMessages = new HashMap<>();
+
+    String userMostMessages = "";
+    int mostMessages = 0;
+
+    for (int i = 0; i < allmessages.size(); i++) {
+      // count for the avg of all messages
+      countM += allmessages.get(i).getText().length();
+
+      // getting the biggest message
+      if (allmessages.get(i).getText().length() > biggest.getText().length()) {
+        biggest = allmessages.get(i);
+      }
+
+
+      // getting and saving the user and message count and checking who has the most sent messages
+      if (!usersAndNumOfMessages.containsKey(allmessages.get(i).getUser())) {
+        usersAndNumOfMessages.put(allmessages.get(i).getUser(), 1);
+        if (1 >= mostMessages) {
+          mostMessages = 1;
+          userMostMessages = allmessages.get(i).getUser();
+        }
+      } else {
+        usersAndNumOfMessages.put(allmessages.get(i).getUser(),
+            usersAndNumOfMessages.get(allmessages.get(i).getUser()) + 1);
+        if (usersAndNumOfMessages.get(allmessages.get(i).getUser()) >= mostMessages) {
+          mostMessages = usersAndNumOfMessages.get(allmessages.get(i).getUser());
+          userMostMessages = allmessages.get(i).getUser();
+        }
+      }
+    }
+
+    /*
+     * Adding properties to Json: Average length of messages Biggest message found Total users List
+     * of users User with most messages sent
+     */
+
+    jsonObject.addProperty("messageAvg",
+        ((allmessages.size() == 0) ? 0 : countM / allmessages.size()));
+    jsonObject.addProperty("biggestMessage", ((biggest == null) ? "" : biggest.getText()));
+    jsonObject.addProperty("usersCount", usersAndNumOfMessages.size());
+    jsonObject.addProperty("usersList",
+        ((usersAndNumOfMessages.size() == 0) ? "" : usersAndNumOfMessages.keySet().toString()));
+    jsonObject.addProperty("mostActiveUser", userMostMessages);
     response.getOutputStream().println(jsonObject.toString());
+
   }
 }
